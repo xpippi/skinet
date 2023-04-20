6ae2fcec463ba74df4733234887d49a49db2b6c5
JackY <thunder_blow@outlook.com>
2023-04-20 18:21:13 +0700

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,25 +10,41 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-      public ProductsController(StoreContext context)
+        private readonly IproductRepository _repo;
+    
+      public ProductsController(IproductRepository repo)
       {
-            this._context = context;
+            this._repo = repo;
+           
         
       }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
-           var products= await _context.Products.ToListAsync() ;
+           var products= await _repo.GetProductByAsync();
 
-           return products;
+           return Ok(products);
         }
 
 
          [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProducts(int id){
 
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
+
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsByAsync());
+        } 
+
+
+          [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypeByAsync());
+        } 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IproductRepository, ProductRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,4 +31,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+await context.Database.MigrateAsync();
+await StoreContextSeed.SeedAsync(context);
+}catch(Exception ex){
+logger.LogError(ex,"An error occired during migration");
+}
 app.Run();  
